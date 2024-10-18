3962b0f7e5e22341ed54df72f96c488094e72b6b
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2024-10-17 21:56:59 -0700

    random updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import "./Navbar.css"
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <div className="nav-container">
-            <ul className="nav-list">
-                <li className="nav-item"><a href="#">Login</a></li>
-               <li className="nav-item"> <a href="#">Register</a></li>
-
-        </ul>
-        </div>
-    );
+  return (
+    <div className="nav-container">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <a href="#">Login</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Register</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
